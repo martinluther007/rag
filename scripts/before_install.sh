d7ad030ac60a089bdd670d259980955c1c43143b
martin luther <martinlutherakuhwa@gmail.com>
2024-07-02 14:34:54 +0100

    first commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 975167783803.dkr.ecr.us-east-1.amazonaws.com
-docker rmi -f 975167783803.dkr.ecr.us-east-1.amazonaws.com/rag-registory:latest
-docker pull 975167783803.dkr.ecr.us-east-1.amazonaws.com/rag-registory:latest
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 975167783803.dkr.ecr.eu-north-1.amazonaws.com
+docker rmi -f 975167783803.dkr.ecr.eu-north-1.amazonaws.com/rag-registory:latest
+docker pull 975167783803.dkr.ecr.eu-north-1.amazonaws.com/rag-registory:latest
--- scripts/before_run.sh
+++ scripts/before_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker rm -f staging-server-latest || true
-docker run --rm 975167783803.dkr.ecr.us-east-1.amazonaws.com/rag-registory:latest npm run migrate:stg:run
-docker run --name staging-server-latest --restart always -d -p 8000:8000 975167783803.dkr.ecr.us-east-1.amazonaws.com/rag-registory:latest
+docker run --rm 975167783803.dkr.ecr.eu-north-1.amazonaws.com/rag-registory:latest npm run migrate:stg:run
+docker run --name staging-server-latest --restart always -d -p 8000:8000 975167783803.dkr.ecr.eu-north-1.amazonaws.com/rag-registory:latest
