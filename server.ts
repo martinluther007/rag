d75b3c24c027bed7b08192db294f616e33240e17
martin luther <martinlutherakuhwa@gmail.com>
2024-07-02 15:02:15 +0100

    first commit

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import server from "./app";
 
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`App is running on port ${port}`);
